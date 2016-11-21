9d6b7c9ed9ac3d1374574f977ab6d42096786497
badhrijagan06 <badhrijagan06@gmail.com>
2016-11-20 20:29:41 -0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,21 @@
+Parse.Cloud.define('sendToUser', function(req, res) {
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo('user', 'badhri');
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  Parse.Push.send({
+    where: query,
+    data: {
+      alert: 'Test',
+      badge: 1,
+      sound: 'default'
+    }
+  }, {
+    success: function() {
+      console.log('##### PUSH OK');
+    },
+    error: function(error) {
+      console.log('##### PUSH ERROR');
+    },
+    useMasterKey: true
+  });
 });
